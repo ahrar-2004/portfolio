b96969f5ee51c77eb44121ad991bb9f7546dc1ce
ahmad <ahmadaliahrar@gmail.com>
2025-03-20 03:27:56 +0500

    versal analytics add

--- src/App.js
+++ src/App.js
@@ -6,24 +6,30 @@ import Experience from "./components/Experience"
 import Projects from "./components/Projects";
 import Services from "./components/Services";
 import Education from "./components/Education";
-
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
+
+
+
 function App() {
   return (
-    <Router> {/* ✅ Wrap everything inside Router */}
-      <Navbar />
-      
-      {/* Sections must have IDs for smooth scrolling */}
-      <section id="home"><Home /></section>
-      <section id="experience"><Experience /></section>
-      <section id="projects"><Projects /></section>
-      <section id="services"><Services /></section>
-      <section id="education"><Education /></section>
-     
-      <section id="contact"><Contact /></section>
-      <section id="footet"><Footer /></section>
-    </Router>
+    <React.Fragment> {/* ✅ Wrap everything inside a single parent */}
+      <Router> {/* ✅ Wrap everything inside Router */}
+        <Navbar />
+        
+        {/* Sections must have IDs for smooth scrolling */}
+        <section id="home"><Home /></section>
+        <section id="experience"><Experience /></section>
+        <section id="projects"><Projects /></section>
+        <section id="services"><Services /></section>
+        <section id="education"><Education /></section>
+       
+        <section id="contact"><Contact /></section>
+        <section id="footet"><Footer /></section>
+      </Router>
+      <Analytics />
+    </React.Fragment>
   );
 }
 
